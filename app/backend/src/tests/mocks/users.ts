c7c913f92fa4b464c331b455c216fcdc283d51b3
eon <bernardomp.dev@gmail.com>
2023-06-23 10:07:00 -0300

    refactor: mock, pkg.json

--- app/backend/src/tests/mocks/users.ts
+++ app/backend/src/tests/mocks/users.ts
@@ -1,14 +1,14 @@
 
 const userMock = {
-  username: "usernameTest",
-  role: "roleTest",
-  email: "email@email.com",
-  password: "passwordTest",
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
 }
 
 const loginMock = {
-  email: "email@email.com",
-  password: "passwordTest",
+  "email": "admin@admin.com",
+  "password": "secret_admin"
 }
 
 export { loginMock, userMock };
