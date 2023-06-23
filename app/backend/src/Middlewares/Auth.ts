2e4a7c447b866dbd6349fd1c3fd539fca13154c0
eon <bernardomp.dev@gmail.com>
2023-06-23 10:09:45 -0300

    refactor: AuthMiddleware.

---
+++ app/backend/src/Middlewares/Auth.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt = require('jsonwebtoken');
+import { JwtPayload } from '../Interfaces/Jwt';
+
+class AuthMiddleware {
+  protected config: jwt.SignOptions;
+  protected secretKey: string;
+
+  constructor() {
+    this.config = { expiresIn: '7d', algorithm: 'HS256' };
+    this.secretKey = process.env.JWT_SECRET || 'secret';
+  }
+
+  public validateToken(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { authorization } = req.headers;
+
+      if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+      const { email, role } = jwt.verify(authorization, this.secretKey) as JwtPayload;
+      res.locals.user = { email, role };
+
+      return next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+  }
+}
+
+export default AuthMiddleware;
