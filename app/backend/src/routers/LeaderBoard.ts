54b826374a0112296efca0927a00aaa6d75a7efe
eon <bernardomp.dev@gmail.com>
2023-06-29 18:12:13 -0300

    feat: GET 'leaderBoard/away' e '/leaderBoard'

--- app/backend/src/routers/LeaderBoard.ts
+++ app/backend/src/routers/LeaderBoard.ts
@@ -4,5 +4,7 @@ import LeaderBoardController from '../controllers/LeaderBoard';
 const leaderBoardRouter = Router();
 
 leaderBoardRouter.get('/home', LeaderBoardController.homeLeaderboard);
+leaderBoardRouter.get('/away', LeaderBoardController.awayLeaderboard);
+leaderBoardRouter.get('/', LeaderBoardController.overallLeaderboard);
 
 export default leaderBoardRouter;
