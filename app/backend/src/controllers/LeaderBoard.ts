01fd9f0057c739b634febd72388f07776e4cf88a
eon <bernardomp.dev@gmail.com>
2023-06-29 18:11:16 -0300

    feat: awayLeaderBoard e overallLeaderBoard in services.

--- app/backend/src/controllers/LeaderBoard.ts
+++ app/backend/src/controllers/LeaderBoard.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 import LeaderBoardService from '../services/LeaderBoard';
-import boardSorter from '../utils/BoardSorter';
+import BoardOverall from '../utils/BoardOverall';
 
 const homeLeaderboard = async (_req: Request, res: Response): Promise<Response> => {
   try {
-    const response = await LeaderBoardService.getleaderboard('homeTeam');
-    response.sort(boardSorter);
+    const response = await LeaderBoardService.getLeaderBoard('homeTeam');
 
     return res.status(200).json(response);
   } catch (error) {
@@ -14,4 +13,34 @@ const homeLeaderboard = async (_req: Request, res: Response): Promise<Response> 
   }
 };
 
-export default { homeLeaderboard };
+const awayLeaderboard = async (_req: Request, res: Response): Promise<Response> => {
+  try {
+    const response = await LeaderBoardService.getLeaderBoard('awayTeam');
+
+    return res.status(200).json(response);
+  } catch (error) {
+    const { message } = error as Error;
+    return res.status(400).json({ message });
+  }
+};
+
+const overallLeaderboard = async (_req: Request, res: Response): Promise<Response> => {
+  try {
+    const homeResponse = await LeaderBoardService.getLeaderBoard('homeTeam');
+    const awayResponse = await LeaderBoardService.getLeaderBoard('awayTeam');
+    const response = [...homeResponse, ...awayResponse];
+
+    const result = BoardOverall(response);
+
+    return res.status(200).json(result);
+  } catch (error) {
+    const { message } = error as Error;
+    return res.status(400).json({ message });
+  }
+};
+
+export default {
+  homeLeaderboard,
+  awayLeaderboard,
+  overallLeaderboard,
+};
