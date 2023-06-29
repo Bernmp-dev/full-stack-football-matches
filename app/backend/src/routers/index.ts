bfce4c347e15c8deda27fb91b85e420481374185
eon <bernardomp.dev@gmail.com>
2023-06-29 12:16:21 -0300

    feat: LeaderBoardRouter.

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import leaderBoardRouter from './LeaderBoard';
 import matchesRouter from './Matches';
 import teamsRouter from './Teams';
 import userRouter from './User';
@@ -8,5 +9,6 @@ const router = Router();
 router.use('/teams', teamsRouter);
 router.use('/login', userRouter);
 router.use('/matches', matchesRouter);
+router.use('/leaderBoard', leaderBoardRouter);
 
 export default router;
